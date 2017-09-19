68c00e5e0d4306695ce32ab2aecca4b98b35438f
Blake Gross <blgross@microsoft.com>
2017-09-19 10:59:00 -0700

    progress on stream changes

---
+++ UnityGLTF/Assets/GLTF/Scripts/ILoader.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+
+using System.IO;
+using System.Collections;
+using GLTFSerialization;
+
+namespace UnityGLTFSerialization
+{
+    public interface ILoader
+    {
+		IEnumerator LoadBuffer(Buffer buffer, System.Action<Stream> streamLoaded);
+		IEnumerator LoadImage(Image image, System.Action<UnityEngine.Texture2D> imageLoaded);
+    }
+}
